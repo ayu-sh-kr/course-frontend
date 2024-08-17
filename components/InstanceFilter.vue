0516aae77687ac468ebfed008421f4a9feec630d
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-17 22:59:05 +0530

    Create InstanceFilter.vue: Filter component to filter out course instance

---
+++ components/InstanceFilter.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+const emit = defineEmits(['onFilter'])
+
+const semesters = [
+    'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII'
+];
+
+const selected = ref();
+const year = ref<string>();
+
+function onFilter() {
+    emit('onFilter', {year: year.value, semester: selected.value})
+}
+
+</script>
+
+<template>
+    <div class="flex items-center gap-x-10">
+        <UInput v-model="year" placeholder="Input Year" />
+        <USelectMenu v-model="selected" :options="semesters" placeholder="Select Semester"/>
+        <UButton @click="onFilter()" label="Filter" class="w-32 flex justify-center"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
