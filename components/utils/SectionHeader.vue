fcd63082114c83bf377426e076d519c58f5f46e7
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-17 21:34:48 +0530

    Create SectionHeader.vue: Common style for section level header

---
+++ components/utils/SectionHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    },
+});
+
+</script>
+
+<template>
+    <div class="relative flex justify-center items-center w-full lg:py-20 md:py-16 py-10">
+        <h1 class="text-4xl font-medium text-gray-800">{{ text }}</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
