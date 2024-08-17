6e57ecdbddf9b62464cc6cfb9c69f98e06574601
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-17 21:29:54 +0530

    Create default.vue: Layout for the app

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {useDark} from "@vueuse/core";
+
+
+const dark = useDark();
+dark.value = false;
+</script>
+
+<template>
+<div class="bg-slate-50">
+  <slot/>
+    <UNotifications/>
+</div>
+</template>
+
+<style scoped>
+
+</style>
