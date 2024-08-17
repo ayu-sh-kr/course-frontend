a69f76774fcda3949169bd57e4508e7b5d4f574b
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-17 21:30:37 +0530

    Create app.d.ts: Contains the types required by the application

---
+++ app.d.ts
@@ -0,0 +1,34 @@
+
+declare interface APIResponse<T> {
+    message: string,
+    description: string,
+    body: T,
+    time: number
+}
+
+declare interface Course {
+    course_id: number,
+    title: string,
+    code: string,
+    description: string,
+    created_on: number
+}
+
+declare interface Instance {
+    id: string,
+    year: string,
+    semester: string,
+    course_id: number,
+    created_on: number
+}
+
+declare interface ColumnMeta {
+    key: string,
+    label: string
+}
+
+declare interface RowActionMeta {
+    label: string,
+    icon: string,
+    click: Function
+}
