57363f1b758b505cf832094a5c947f6e74ceff0a
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-17 21:35:12 +0530

    Create Scaffold.vue: Scaffold for pre design section

---
+++ components/utils/Scaffold.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<section class="font-aktive grid grid-rows-[auto_1fr] h-screen overflow-hidden">
+  <slot/>
+</section>
+</template>
+
+<style scoped>
+
+</style>
